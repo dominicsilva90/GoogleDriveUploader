e7690b0fad6f7241d76c6675ba1d0762b6dd9b88
Dominic <dominic@plates4less.co.uk>
2020-08-25 11:35:03 +0100

    Add project files.

---
+++ uploader/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace uploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(167, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(12, 63);
+            this.resultLabel.MaximumSize = new System.Drawing.Size(226, 0);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 12);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 96);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(270, 135);
+            this.MinimumSize = new System.Drawing.Size(270, 135);
+            this.Name = "Form1";
+            this.Text = "EZ Uploader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ uploader/Form1.cs
@@ -0,0 +1,132 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using HeyRed.Mime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uploader
+{
+    public partial class Form1 : Form
+    {
+        static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "Quick Uploader";
+        public DriveService service { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = GetCredentials(),
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string pageToken = null;
+            do
+            {
+                var response = UploadFile(service, textBox1.Text, "TEST");
+                resultLabel.ForeColor = Color.ForestGreen;
+                resultLabel.Text = response.Name + " uploaded.";
+            }
+            while (pageToken != null);
+            textBox1.Clear();
+        }
+        private UserCredential GetCredentials()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+            return credential;
+        }
+        private Google.Apis.Drive.v3.Data.File UploadFile(DriveService service, string uploadFile, string description)
+        {
+            if (File.Exists(uploadFile))
+            {
+                Google.Apis.Drive.v3.Data.File body = new Google.Apis.Drive.v3.Data.File();
+                string mimeType = GetMimeType(uploadFile);
+                body.Name = Path.GetFileName(uploadFile);
+                body.Description = description;
+                body.MimeType = mimeType;
+                byte[] byteArray = System.IO.File.ReadAllBytes(uploadFile);
+                MemoryStream stream = new MemoryStream(byteArray);
+                try
+                {
+                    FilesResource.CreateMediaUpload request = service.Files.Create(body, stream, mimeType);
+                    request.SupportsTeamDrives = true;
+                    request.Upload();
+                    return request.ResponseBody;
+
+                }
+                catch (Exception e)
+                {
+                    resultLabel.Text = e.Message;
+                    return new Google.Apis.Drive.v3.Data.File();
+                }
+            }
+            else
+            {
+                resultLabel.Text = "File does not exist";
+                return new Google.Apis.Drive.v3.Data.File();
+            }
+
+        }
+
+        private string GetMimeType(string fileName)
+        {
+            return MimeGuesser.GuessMimeType(fileName);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                button2.Enabled = false;
+            }
+            else
+            {
+                button2.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
